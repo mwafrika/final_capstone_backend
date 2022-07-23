f4a9a28f0833f4380002a264db6e02c6673b213d
mwafrika <25848053+mwafrika@users.noreply.github.com>
2022-07-24 01:29:09 +0300

    Add crud for location

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -1,2 +1,36 @@
 class LocationsController < ApplicationController
+    before_action :set_location, only: %i[show update destroy]
+
+    include Response
+    def index
+        @locations = Location.all
+        json_response(@locations)
+    end
+
+    def show
+        json_response(@location)
+    end
+    
+    def create
+        @location = Location.create!(location_params)
+        json_response(@location, :created)
+    end
+
+    def update
+        @location.update(location_params)
+        head :no_content
+    end
+
+    def destroy
+        @location.destroy
+        head :no_content
+    end
+
+    def set_location
+        @location = Location.find(params[:id])
+    end
+
+    def location_params
+        params.permit(:name, :address, :city, :state, :zip, :phone, :website, :image, :description)
+    end
 end
---
+++ spec/requests/bikes_spec.rb
@@ -0,0 +1,151 @@
+require 'swagger_helper'
+
+RSpec.describe 'bikes', type: :request do
+
+  path '/latest' do
+
+    get('latest bike') do
+      response(200, 'successful') do
+
+        after do |example|
+          example.metadata[:response][:content] = {
+            'application/json' => {
+              example: JSON.parse(response.body, symbolize_names: true)
+            }
+          }
+        end
+        run_test!
+      end
+    end
+  end
+
+  path '/' do
+
+    get('list bikes') do
+      response(200, 'successful') do
+
+        after do |example|
+          example.metadata[:response][:content] = {
+            'application/json' => {
+              example: JSON.parse(response.body, symbolize_names: true)
+            }
+          }
+        end
+        run_test!
+      end
+    end
+  end
+
+  path '/bikes' do
+
+    get('list bikes') do
+      response(200, 'successful') do
+
+        after do |example|
+          example.metadata[:response][:content] = {
+            'application/json' => {
+              example: JSON.parse(response.body, symbolize_names: true)
+            }
+          }
+        end
+        run_test!
+      end
+    end
+
+    post('create bike') do
+      response(200, 'successful') do
+
+        after do |example|
+          example.metadata[:response][:content] = {
+            'application/json' => {
+              example: JSON.parse(response.body, symbolize_names: true)
+            }
+          }
+        end
+        run_test!
+      end
+    end
+  end
+
+  path '/bikes/new' do
+
+    get('new bike') do
+      response(200, 'successful') do
+
+        after do |example|
+          example.metadata[:response][:content] = {
+            'application/json' => {
+              example: JSON.parse(response.body, symbolize_names: true)
+            }
+          }
+        end
+        run_test!
+      end
+    end
+  end
+
+  path '/bikes/{id}' do
+    # You'll want to customize the parameter types...
+    parameter name: 'id', in: :path, type: :string, description: 'id'
+
+    get('show bike') do
+      response(200, 'successful') do
+        let(:id) { '123' }
+
+        after do |example|
+          example.metadata[:response][:content] = {
+            'application/json' => {
+              example: JSON.parse(response.body, symbolize_names: true)
+            }
+          }
+        end
+        run_test!
+      end
+    end
+
+    patch('update bike') do
+      response(200, 'successful') do
+        let(:id) { '123' }
+
+        after do |example|
+          example.metadata[:response][:content] = {
+            'application/json' => {
+              example: JSON.parse(response.body, symbolize_names: true)
+            }
+          }
+        end
+        run_test!
+      end
+    end
+
+    put('update bike') do
+      response(200, 'successful') do
+        let(:id) { '123' }
+
+        after do |example|
+          example.metadata[:response][:content] = {
+            'application/json' => {
+              example: JSON.parse(response.body, symbolize_names: true)
+            }
+          }
+        end
+        run_test!
+      end
+    end
+
+    delete('delete bike') do
+      response(200, 'successful') do
+        let(:id) { '123' }
+
+        after do |example|
+          example.metadata[:response][:content] = {
+            'application/json' => {
+              example: JSON.parse(response.body, symbolize_names: true)
+            }
+          }
+        end
+        run_test!
+      end
+    end
+  end
+end

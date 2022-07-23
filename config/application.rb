8d0443c9eaa33ce93833216cebb95532150ee4fa
mwafrika <25848053+mwafrika@users.noreply.github.com>
2022-07-23 17:31:11 +0300

    Change secret key

--- config/application.rb
+++ config/application.rb
@@ -11,7 +11,7 @@ module BackendeApi
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
     config.assets.initialize_on_precompile = false
-    
+
     config.session_store :cookie_store, key: '_interslice_session'
     config.middleware.use ActionDispatch::Cookies
     config.middleware.use config.session_store, config.session_options

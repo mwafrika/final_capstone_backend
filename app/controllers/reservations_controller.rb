cd9a8555db519aefd04474d42e9b96fb79c3a839
bushmusi <bushera.mestofa@gmail.com>
2022-07-23 11:20:53 +0300

    Reservation add fetch and create endpoint

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -4,4 +4,19 @@ class ReservationsController < ApplicationController
     @data = Reservation.all
     json_response(@data)
   end
+
+  def create
+    @reserve = current_user.reservations.create!(allowed_params)
+
+    if @reserve.save
+      json_response(@reserve, :created)
+    else
+      json_response(@reserve.errors, :unprocessable_entity)
+    end
+  end
+
+  def allowed_params
+    params.permit(:reservation_number, :date_reserved,
+                  :location_id, :bike_id,  user_id: current_user.id)
+  end
 end

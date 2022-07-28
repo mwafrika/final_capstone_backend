eda708f499d20418797b28e44cb9d4e2cee6e643
bushmusi <bushera.mestofa@gmail.com>
2022-07-28 17:30:18 +0300

    Make reservation col update

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -1,8 +1,7 @@
 class Reservation < ApplicationRecord
   belongs_to :bike
-  belongs_to :location
   belongs_to :user
 
-  validates :reservation_number, presence: true, numericality: { in: 1..5 }
+  validates :reservation_number, presence: true, numericality: { in: 1..15 }
   validates :date_reserved, presence: true
 end
